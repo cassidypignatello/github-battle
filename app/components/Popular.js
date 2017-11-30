e111da1f42e8bd0bff097f6b61cca3642bcea718
cassidypignatello <cassidy.pignatello@gmail.com>
2017-11-29 20:02:54 -0500

    selected languaged updates within popular ui

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang) {
+    this.setState(function() {
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
   render() {
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return (
-      <div>Popular!</div>
+      <ul className='languages'>
+        <p>Selected Language: {this.state.selectedLanguage}</p>
+        {languages.map((lang) => <li key={lang} onClick={this.updateLanguage.bind(null, lang)}>{lang}</li>)}
+      </ul>
     );
   }
 }

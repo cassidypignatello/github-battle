43ced7c5ca5edc942d09c14bf36b14bf44337692
cassidypignatello <cassidy.pignatello@gmail.com>
2017-11-30 20:54:01 -0500

    refactors Popular ui into its own stateless functional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className='languages'>
+      {languages.map(function(lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+              {lang}
+          </li>
+        )
+      })}
+    </ul>
+  );
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
 
 export default class Popular extends React.Component {
   constructor(props) {
@@ -16,16 +40,13 @@ export default class Popular extends React.Component {
     });
   }
   render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return (
-      <ul className='languages'>
-        {languages.map((lang) => 
-          <li key={lang} 
-              onClick={this.updateLanguage.bind(null, lang)} 
-              style={lang === this.state.selectedLanguage ? { color: '#d0021b' } : null}>
-              {lang}
-          </li>)}
-      </ul>
+      <div>
+        <SelectLanguage 
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
     );
   }
 }

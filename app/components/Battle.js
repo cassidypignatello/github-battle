a7a7419c8f497124c3a050ca405e0e15661f705f
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-04 09:40:37 -0500

    adds state and handleSubmit function to Battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    };
+  }
+  handleSubmit = (id, username) => {
+    this.setState({
+      let newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github/com' + username + '.png?size=200';
+      return newState;
+    });
+  }
   render () {
+    let playerOneName = this.state.playerOneName;
+    let playerTwoName = this.state.playerTwoName;
     return (
       <div>
-        BATTLE!
+        <div className='row'>
+          {!playerOneName &&
+            <PlayerInput 
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit} 
+            />}
+
+          {!playerTwoName &&
+            <PlayerInput
+              id='playerTwo'
+              label='Player Two'
+              onSubmit={this.handleSubmit} 
+            />}
+        </div>
       </div>
     );
   }

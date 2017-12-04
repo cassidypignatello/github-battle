114aa8f023fe15692b82479e51038539b2962490
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-04 16:25:44 -0500

    creates controlled form for PlayerInput

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,61 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    var value = event.target.value;
+    this.setState(function() {
+      return {
+        username: value
+      }
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    )
+  }
+  render() {
+    return (
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>
+          {this.props.label}
+        </label>       
+        <input
+          id='username'
+          placeholder='Github Username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange}
+        /> 
+        <button 
+          type="submit" 
+          className="button"
+          disabled={!this.state.username}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+   id: PropTypes.string.isRequired,
+   label: PropTypes.string.isRequired,
+   onSubmit: PropTypes.func.isRequired
+}
 
 export default class Battle extends React.Component {
   constructor(props) {
@@ -9,9 +66,10 @@ export default class Battle extends React.Component {
       playerOneImage: null,
       playerTwoImage: null
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleSubmit = (id, username) => {
-    this.setState({
+  handleSubmit(id, username) {
+    this.setState(function() {
       let newState = {};
       newState[id + 'Name'] = username;
       newState[id + 'Image'] = 'https://github/com' + username + '.png?size=200';

588780d41182e9c244fe784ec215926cc6066a1b
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-05 21:34:52 -0500

    builds Profile component for results screen

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,10 +5,33 @@ import api from '../utils/api';
 import { Link } from 'react-router-dom';
 import PlayerPreview from './PlayerPreview';
 
+const Profile  = (props) => {
+  let info = props.info;
+  return (
+    <PlayerPreview avatar={info.avatar_url} username={info.login}>
+      <ul className='space-list-items'>
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+      </ul>
+    </PlayerPreview>
+  );
+}
+
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired
+}
+
 const Player = (props) =>  (
   <div>
     <h1 className='header'>{props.label}</h1>
     <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
+    <Profile info={props.profile}/>
   </div>
 );
 

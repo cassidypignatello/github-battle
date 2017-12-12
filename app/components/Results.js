1878a182fe293e1245ab577461fae91c3bf671f5
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-12 18:22:21 -0500

    refactors Results to use ES6 features

--- app/components/Results.js
+++ app/components/Results.js
@@ -6,33 +6,32 @@ import { Link } from 'react-router-dom';
 import PlayerPreview from './PlayerPreview';
 import Loading from './Loading';
 
-const Profile  = (props) => {
-  let info = props.info;
+const Profile = (props) => {
+  const { avatar_url, login, name, location, company, followers, following, public_repos, blog } = props.info;
   return (
-    <PlayerPreview avatar={info.avatar_url} username={info.login}>
+    <PlayerPreview avatar={avatar_url} username={login}>
       <ul style={{textAlign: 'center'}} className='space-list-items'>
-        {info.name && <li>{info.name}</li>}
-        {info.location && <li>{info.location}</li>}
-        {info.company && <li>{info.company}</li>}
-        <li>Followers: {info.followers}</li>
-        <li>Following: {info.following}</li>
-        <li>Public Repos: {info.public_repos}</li>
-        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+        {name && <li>{name}</li>}
+        {location && <li>{location}</li>}
+        {company && <li>{company}</li>}
+        <li>Followers: {followers}</li>
+        <li>Following: {following}</li>
+        <li>Public Repos: {public_repos}</li>
+        {blog && <li><a href={blog}>{blog}</a></li>}
       </ul>
     </PlayerPreview>
   );
 }
 
-
 Profile.propTypes = {
   info: PropTypes.object.isRequired
 }
 
-const Player = (props) =>  (
+const Player = ({ label, score, profile }) =>  (
   <div>
-    <h1 className='header'>{props.label}</h1>
-    <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-    <Profile info={props.profile}/>
+    <h1 className='header'>{label}</h1>
+    <h3 style={{textAlign: 'center'}}>Score: {score}</h3>
+    <Profile info={profile}/>
   </div>
 );
 
@@ -53,34 +52,27 @@ export default class Results extends React.Component {
     };
   }
   componentDidMount() {
-    let players = queryString.parse(this.props.location.search);
+    const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
     api.battle([
-      players.playerOneName,
-      players.playerTwoName
+      playerOneName,
+      playerTwoName
     ]).then((results) => {
       if (results === null) {
-        return this.setState(function () {
-          return {
-            error: 'Looks like there was an error. Check that both users exist on Github',
-            loading: false
-          }
+        return this.setState({
+          error: 'Looks like there was an error. Check that both users exist on Github',
+          loading: false
         });
       }
-      this.setState(function() {
-        return {
-          error: null,
-          winner: results[0],
-          loser: results[1],
-          loading: false
-        }
-      })
+      this.setState({
+        error: null,
+        winner: results[0],
+        loser: results[1],
+        loading: false
+      });
     });
   }
   render() {
-    let error = this.state.error;
-    let winner = this.state.winner;
-    let loser = this.state.loser;
-    let loading = this.state.loading;
+    const { winner, loser, error, loading } = this.state;
 
     if (loading === true) {
       return (

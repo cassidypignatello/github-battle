7ad23fe8d980cb387ff0b9fc2f8a909485bcfc50
cassidypignatello <cassidy.pignatello@gmail.com>
2017-12-21 14:49:23 -0500

    removes constructor from Results

--- app/components/Results.js
+++ app/components/Results.js
@@ -42,15 +42,12 @@ Player.propTypes = {
 }
 
 export default class Results extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      winner: null,
-      loser: null,
-      error: null,
-      loading: true
-    };
-  }
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
+  };
   componentDidMount() {
     const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
     battle([
